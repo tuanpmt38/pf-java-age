b5697d0351e45c797ef088379ce08171e4845cca
tuanpmt38 <37221796+tuanpmt38@users.noreply.github.com>
2018-03-16 22:20:56 +0700

    age tuoi

---
+++ Age.java
@@ -0,0 +1,14 @@
+public class Age {
+    public static void main(String[] args) {
+        int age;
+        int year = 2018;
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+        System.out.println("Enter year:");
+        year = sc.nextInt();
+        age = 2018 - year;
+        if (year >= 2018)
+            System.out.println("Enter false");
+        else
+            System.out.println("Display Age:" + age);
+    }
+}
